669c7ba11ea6635dadb9cf00eb29d26b77941ac8
Amyleep <aleep1997@outlook.com>
2024-06-17 18:23:35 +1000

    Building a simple calculator and introducting lists

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python Term 1 Week 6 Saturday class
+
+# If else Practice activity
+
+# Building a calculator
+
+Build a calculator that takes 3 inputs from the user:
+2 inputs as numbers (Floats) for operations and 3rd input to decide which operation they want to perform.
+
+# Lists
+Composite Data type, meaning multiple data types can ne stored in one variable.
+Each piece is called an element.
---
+++ list_example.py
@@ -0,0 +1,25 @@
+# Country1 = "Australia"
+# Country2 = "New Zealand"
+# Country3 = "USA"
+# Country4 = "England"
+
+# print(Country1)
+# print(Country2)
+# print(Country3)
+# print(Country4)
+
+Countries = ["Australia", "New Zealand", "USA", "England"]
+
+Countries[1] = "South Africa"
+
+print(Countries)
+
+Countries.append("New Zealand")
+
+print(Countries)
+
+Countries.remove("USA")
+
+print(Countries)
+
+print("total length of the list:", len (Countries))
